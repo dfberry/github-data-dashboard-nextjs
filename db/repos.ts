dc576ff5638a3e4af0bf0924597911775abae787
Dina Berry <dinaberry@outlook.com>
2023-02-20 10:50:26 -0800

    still working on error for ssg

--- db/repos.ts
+++ db/repos.ts
@@ -1,36 +1,28 @@
 export async function repos() {
+  try {
+    const base = process.env.REACT_APP_FN_BASE;
+    const code = process.env.REACT_APP_FN_ORG_CODE;
 
-try{
+    if (!base) throw Error("REACT_APP_FN_BASE is empty");
+    if (!code) throw Error("REACT_APP_FN_ORG_CODE is empty");
 
-  const base = process.env.REACT_APP_FN_BASE;
-  const code = process.env.REACT_APP_FN_ORG_CODE;
+    console.log(`base length = ${base.length}`);
+    console.log(`code = ${code.length}`);
 
-  if(!base) throw Error("REACT_APP_FN_BASE is empty")
-  if(!code) throw Error("REACT_APP_FN_ORG_CODE is empty")
+    const url = `${base}/org?code=${code}`;
+    console.log(`org url = ${url.length}`);
 
-  console.log(`base length = ${base.length}`)
-  console.log(`code = ${code.length}`)
+    const response = await fetch(url);
 
-  const url = `${base}/org?code=${code}`;
-  console.log(`org url = ${url.length}`)
-
-  const response = await fetch(url);
-
-  if (response.ok) {
-    const json: any = await response.json();
-    console.log(`repos length=${json.length}`);
-    return json;
-  } else {
-    const error: any = await response.text();
-    console.log(`repos fetch error ${JSON.stringify(error)}`)
+    if (response.ok) {
+      const json: any = await response.json();
+      console.log(`repos length=${json.length}`);
+      return json;
+    } else {
+      throw Error(`${response.status}: ${response.statusText}`);
+    }
+  } catch (err) {
+    console.log(`repos function error = ${err}`);
     return [];
   }
-}catch(err){
-    console.log(`repos function error = ${JSON.stringify(err)}`)
-    return [];
-}
-
-  // json.map((repo:any):any =>{
-  //   return repo.repositoryName = repo.repositoryName.toLowerCase()
-  // })
 }
